7d3a07f5d3e5f68a3330b56d5ead82fbfb5316b2
andteo <andreiteodoru@aol.com>
2023-05-23 08:44:10 +0300

    updates

--- app/src/main/java/com/ivanmagda/habito/util/HabitoStringUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.ivanmagda.habito.util
-
-class HabitoStringUtils {
-    companion object {
-        fun capitalized(string: String): String {
-            return string.substring(0, 1).toUpperCase() + string.substring(1)
-        }
-    }
-}
